a4e9530451013c2fd5041654eead4d6f1141a8c6
Akshay Dubey <akshay.dubey055@gmail.com>
2022-11-14 15:40:11 +0530

    Add custom in-memory user

---
+++ src/main/java/com/github/itsAkshayDubey/springsecuritydemo/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.github.itsAkshayDubey.springsecuritydemo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfiguration {
+	
+	@Bean
+	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+		return new InMemoryUserDetailsManager(
+				User.withUsername("test").password(passwordEncoder().encode("secret")).roles("USER").build(),
+				User.withUsername("admin").password(passwordEncoder().encode("adminsecret")).roles("ADMIN").build()
+				);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
