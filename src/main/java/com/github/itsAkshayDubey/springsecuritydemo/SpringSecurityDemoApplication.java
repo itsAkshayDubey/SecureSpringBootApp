1a84067de7ff48d82ece1c06d76c4b4eab6db125
Akshay Dubey <akshay.dubey055@gmail.com>
2022-11-15 12:34:12 +0530

    Handle http to https redirection

--- src/main/java/com/github/itsAkshayDubey/springsecuritydemo/SpringSecurityDemoApplication.java
+++ src/main/java/com/github/itsAkshayDubey/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -1,7 +1,16 @@
 package com.github.itsAkshayDubey.springsecuritydemo;
 
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class SpringSecurityDemoApplication {
@@ -9,5 +18,28 @@ public class SpringSecurityDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityDemoApplication.class, args);
 	}
+	
+	 @Bean
+	    public ServletWebServerFactory servletContainer() {
+	        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+	            protected void postProcessContext(Context context) {
+	                SecurityConstraint securityConstraint = new SecurityConstraint();
+	                securityConstraint.setUserConstraint("CONFIDENTIAL");
+	                SecurityCollection collection = new SecurityCollection();
+	                collection.addPattern("/*");
+	                securityConstraint.addCollection(collection);
+	                context.addConstraint(securityConstraint);
+	            }
+	        };
+	        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+	        return tomcat;
+	    }
 
+	    private Connector redirectConnector() {
+	        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	        connector.setScheme("http");
+	        connector.setPort(8080);
+	        connector.setRedirectPort(8443);
+	        return connector;
+	    }
 }
